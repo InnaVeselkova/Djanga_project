327ed232a4ad8f3e008aa96c8e7a5ed925865987
Inna <MenelN@yandex.ru>
2025-11-18 23:58:56 +0300

    add blog

--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,21 @@ class BlogListView(ListView):
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
 
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = BlogModel
     template_name = 'blog/blog_detail.html'
     context_object_name = 'blog'
     pk_url_kwarg = 'blog_id'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count = (obj.views_count or 0) + 1
+        obj.save(update_fields=['views_count'])
+        return obj
 
 
 class BlogCreateView(CreateView):
@@ -28,8 +37,10 @@ class BlogUpdateView(UpdateView):
     model = BlogModel
     fields = ['title', 'content', 'preview_image', 'is_published']
     template_name = 'blog/blog_form.html'
-    success_url = reverse_lazy('blog:blog_list')
     pk_url_kwarg = 'blog_id'
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blog_detail', kwargs={'blog_id': self.object.id})
 
 
 class BlogDeleteView(DeleteView):
