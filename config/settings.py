afa1ac1baa0cf85168bfade8579dc29e0d94ef71
Inna <MenelN@yandex.ru>
2025-12-08 16:35:44 +0300

    add corrections

--- config/settings.py
+++ config/settings.py
@@ -135,8 +135,10 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'MenelN@yandex.ru'
-EMAIL_HOST_PASSWORD = 'unezfxuxefxxewyd'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = 'catalog:home'
+
+LOGIN_URL = "users:login"
--- users/views.py
+++ users/views.py
@@ -6,12 +6,6 @@ from django.core.mail import send_mail
 from django.contrib.auth import login
 
 
-class RegisterView(CreateView):
-    template_name = 'users/register.html'
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('catalog:home')
-
-
 class RegisterView(FormView):
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
